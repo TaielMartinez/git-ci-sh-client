e3ad79afa3edd73ef96ffdb84d05bb4ec413cb8f
TaielMG <taielmartinezgort@gmail.com>
2022-10-16 02:01:09 -0300

    fix install

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sudo apt-get install nodejs
 sudo apt-get install npm
-npm install
 sudo npm install pm2@latest -g
 pm2 stop 0
 pm2 delete 0
@@ -8,4 +7,5 @@ git add *
 git stash
 git reset --hard
 git pull
+npm install
 pm2 start client.js
